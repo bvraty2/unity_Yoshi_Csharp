f769b4cce4177303fab87a719c3ad346b6d61d5e
bvraty2 <63327363+bvraty2@users.noreply.github.com>
2020-04-08 15:29:47 +0800

    認識欄位

--- unity_Yoshi_Csharp/Assets/class1.cs
+++ unity_Yoshi_Csharp/Assets/class1.cs
@@ -11,6 +11,14 @@ public class class1 : MonoBehaviour
     // 1. 大小不一樣
     // 2. 括號成對出現 () [] {} '' "" < >
 
+    [Header("汽車一號")]
+    public Car car1;
+
+    [Header("汽車二號")]
+    public Car car2;
+
+
+    
     // 事件：在特定時間會以指定次數執行的方法 (名稱為藍色)
     // 開始事件：播放遊戲時執行一次 - 初始設定
     private void Start()
@@ -18,5 +26,16 @@ public class class1 : MonoBehaviour
         // 輸出("文字訊息")
         // C# 結尾為分號 ;
         print("ㄩㄇ");
+
+        // 取得 get
+        print("汽車一號的CC數是"+ car1.cc);
+        print("汽車二號的品牌是"+ car2.brand);
+
+        // 設定 set
+        car1.cc = 678;
+        car1.brake = true;
+        car1.weight = 666.6f;
+        car1.brand = "gogoro";
+
     }
 }
